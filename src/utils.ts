10ee5042bf625a88613b2055940b18ae4d09106e
dominikbubu <czanerdominik@gmail.com>
2022-07-11 16:19:10 +0200

    Changed request function.

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 const request = (url: string) => {
-  return fetch(url).then((response) => response.json());
+  const data = fetch(url).then((response) => response.json());
+  return data;
 };
 
 export { request };
